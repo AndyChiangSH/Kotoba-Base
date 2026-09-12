bdd17673f9a51325a0aaec3cb365eb3f3bfab0c7
AndyChiangSH <andy10801@gmail.com>
2026-09-12 12:29:29 +0800

    v1.1

--- src/App.tsx
+++ src/App.tsx
@@ -737,7 +737,7 @@ function App() {
               <img src="/icon-2.png" alt="" />
             </span>
             <span>日文單字庫</span>
-            <span className="version-badge">v1.0</span>
+            <span className="version-badge">v1.1</span>
           </div>
           <button
             className="icon-button"
